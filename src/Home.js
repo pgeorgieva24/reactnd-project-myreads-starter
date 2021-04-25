c884611f052a865b9ede5d49ca412b3b11b90c3b
Plamena Georgieva <pgeorgieva24@yahoo.com>
2021-04-25 10:25:58 +0300

    fix: Add key to BookShelf and make component functional

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf'
 
-class Home extends Component {
-    render() {
-        const { books, shelfChanged } = this.props;
-        const shelfs = [];
+const Home = ({ books, shelfChanged }) => {
+    const shelfs = [];
 
-        books.forEach(book => {
-            if (shelfs.indexOf(book.shelf) < 0) {
-                shelfs.push(book.shelf);
-            }
-        })
+    books.forEach(book => {
+        if (shelfs.indexOf(book.shelf) < 0) {
+            shelfs.push(book.shelf);
+        }
+    })
 
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    {shelfs.map(shelf => (
-                        <BookShelf booksOnShelf={books.filter(book => book.shelf === shelf)} shelf={shelf} shelfChanged={shelfChanged} />
-                    ))}
-                </div>
-                <Link
-                    to='/search'
-                    className='open-search'
-                >Add a book</Link>
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
             </div>
-        )
-    }
+            <div className="list-books-content">
+                {shelfs.map(shelf => (
+                    <BookShelf key={shelf} booksOnShelf={books.filter(book => book.shelf === shelf)} shelf={shelf} shelfChanged={shelfChanged} />
+                ))}
+            </div>
+            <Link
+                to='/search'
+                className='open-search'
+            >Add a book</Link>
+        </div>
+    )
 }
 
 export default Home
